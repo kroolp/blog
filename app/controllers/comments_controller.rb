b0f4c58ee3a8b95aeb8192d7ac1f0bf35990177d
Piotr Król <piotr.krol@codesthq.com>
2018-03-04 10:45:08 +0100

    rember commenter

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params.merge(article: @article))
     if @comment.save
+      session[:commenter] = @comment.commenter
       redirect_to article_path(@article)
     else
       render 'articles/show'
