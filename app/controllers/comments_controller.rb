69ebdaa0fa8b0d8062e440161af1252646e6b075
Piotr Król <piotr.krol@codesthq.com>
2018-03-04 15:34:24 +0100

    authorize article actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: 'admin', password: 'secret', only: [:destroy]
   before_action :provide_article, only: [:create, :destroy]
 
   def create
